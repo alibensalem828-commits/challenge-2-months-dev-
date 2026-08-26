fbce4c0aa672397ae9736dacc465e8e49bb29eed
MissingNo0 <alibensalem828@gmail.com>
2026-08-26 17:37:15 +0200

    let et const en poche

---
+++ js/base.js
@@ -0,0 +1,13 @@
+let age = 18;
+console.log(age);
+
+console.log("hello")
+let yassine="sakar salli" ;
+yassine=30
+console.log(yassine)
+const age_verrouille= 2008;
+console.log(age_verrouille)
+const nom="ali" ;
+const prix=39;
+const estmajeur=true;
+console.log(prix,estmajeur)
