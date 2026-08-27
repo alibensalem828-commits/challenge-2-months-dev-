9ceb70764deae9b6e8751b8542d8c85e16d04d48
MissingNo0 <alibensalem828@gmail.com>
2026-08-27 18:55:02 +0200

    les boucle

---
+++ js/boucle.js
@@ -0,0 +1,17 @@
+for (let x=0;x<3;x=1+ x)
+    {
+        console.log(x) ;
+    }
+
+let compteur =0 ;
+while (compteur<6){
+    console.log(compteur);
+    compteur+=1
+} 
+let x=100;
+for(x;x>=6
+    ;x--
+){console.log(x);
+if(x===80){break;}
+
+}
