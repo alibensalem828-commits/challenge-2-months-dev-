0358ea3c3fdc3873b15917143e849a700bb9906a
MissingNo0 <alibensalem828@gmail.com>
2026-08-29 16:08:00 +0200

    les objet-cle-valeur-

---
+++ js/lesobjets.js
@@ -0,0 +1,11 @@
+let personne = {
+    age:23,
+    prenom:'ali',
+    profession:'etudian',
+};
+console.log(personne.age);
+console.log(personne[ "prenom" ,'age']);
+
+personne.age=19;
+personne.ville='paris';
+console.log(personne)
