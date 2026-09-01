3b70ffd8051c679ccda86662eb443e77e19df6d5
MissingNo0 <alibensalem828@gmail.com>
2026-09-01 20:26:52 +0200

    dom1

--- js/dom/js-dom.js
+++ js/dom/js-dom.js
@@ -7,17 +7,5 @@ titre.textContent="jai change le titre avec js !"
 let liste = document.querySelector("#liste")
 liste.textContent='liste de la recherche queyselector'
 let item=document.createElement("li") ;
+item.textContent='test liste 2'
 liste.append(item)
-let produits = [
-    { nom: "Clavier", prix: 50 },
-    { nom: "Souris", prix: 25 },
-    { nom: "Écran", prix: 200 }
-];
-
-let liste = document.querySelector("#liste");
-
-produits.forEach(produit => {
-    let item = document.createElement("li");             // 1. CRÉER (un <li> par produit)
-    item.textContent = `${produit.nom} : ${produit.prix} €`;  // 2. REMPLIR avec les infos
-    liste.append(item);                                   // 3. ACCROCHER dans la liste
-});
